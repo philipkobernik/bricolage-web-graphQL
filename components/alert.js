37f4fea73d315e94f5e17a5425f8d10e3096df6a
Philip Kobernik <ptk921@gmail.com>
2020-05-29 19:32:25 -0700

    remove the link to github repo at the top of the page

--- components/alert.js
+++ components/alert.js
@@ -3,7 +3,9 @@ import cn from 'classnames'
 import { EXAMPLE_PATH } from '../lib/constants'
 
 export default function Alert({ preview }) {
-  return (
+  // if(!preview) return null;
+
+  return preview ? (
     <div
       className={cn('border-b', {
         'bg-accent-7 border-accent-7 text-white': preview,
@@ -12,31 +14,20 @@ export default function Alert({ preview }) {
     >
       <Container>
         <div className="py-2 text-center text-sm">
-          {preview ? (
-            <>
-              This is page is a preview.{' '}
-              <a
-                href="/api/exit-preview"
-                className="underline hover:text-cyan duration-200 transition-colors"
-              >
-                Click here
-              </a>{' '}
-              to exit preview mode.
-            </>
-          ) : (
-            <>
-              The source code for this blog is{' '}
-              <a
-                href={`https://github.com/zeit/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-                className="underline hover:text-success duration-200 transition-colors"
-              >
-                available on GitHub
-              </a>
-              .
-            </>
-          )}
+
+          <>
+            This is page is a preview.{' '}
+            <a
+              href="/api/exit-preview"
+              className="underline hover:text-cyan duration-200 transition-colors"
+            >
+              Click here
+            </a>{' '}
+            to exit preview mode.
+          </>
+
         </div>
       </Container>
     </div>
-  )
+  ) : null;
 }
