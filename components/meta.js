d1d252faf20207efa8ccc0d80ce4b74a03375588
sam bourgault <samuelle.bourgault@gmail.com>
2020-06-10 16:22:40 -0700

    added all the favicon images

--- components/meta.js
+++ components/meta.js
@@ -13,13 +13,13 @@ export default function Meta() {
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/favicon/favicon_brico_32x32.png"
+        href="/favicon/favicon-32x32.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/favicon/favicon_brico_16x16.png"
+        href="/favicon/favicon-16x16.png"
       />
       <link rel="manifest" href="/favicon/site.webmanifest" />
       <link
