c2021d8ae6fdfff9fa69d1410bd11bc66729b5d0
sdinulescu <stejara@gmail.com>
2020-06-12 12:04:38 -0700

    Beginning of Node Navi
    At this point, only a black rectangle is displayed on the homepage. Functionality will be built in step-by-step.

--- components/intro.js
+++ components/intro.js
@@ -1,5 +1,6 @@
 import { CMS_NAME, CMS_URL } from '../lib/constants'
 import BricolageLogo from '../components/bricolage-logo'
+import BricolageNodeNavi from '../components/bricolage-nodeNavi'
 
 
 export default function Intro() {
@@ -7,6 +8,7 @@ export default function Intro() {
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         <BricolageLogo />
+        <BricolageNodeNavi />
       </h1>
       { false && (
         <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
@@ -14,5 +16,18 @@ export default function Intro() {
         </h4>
       )}
     </section>
+
   )
+        // <div class="grid grid-rows-1 grid-flow-col gap-4">
+        // <div class="grid grid-flow-row grid-cols-2 gap-4">
+        //   <div class="row-span-1 col-span-1 ..."></div>
+        //   <BricolageLogo />
+        //   <div class="row-span-1 col-span-1 ..."></div>
+        //   <BricolageNodeNavi />
+        // </div>
+
+        //<div className="mb-8 md:mb-16">
+
+
+  
 }
