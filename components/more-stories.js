09291b4309d76af4133260b111e16951e0f4ee7e
Philip Kobernik <ptk921@gmail.com>
2020-06-17 16:33:13 -0700

    make headline a prop-- default is just "Projects"

--- components/more-stories.js
+++ components/more-stories.js
@@ -1,10 +1,10 @@
 import ProjectPreview from '../components/project-preview'
 
-export default function MoreStories({ projects }) {
+export default function MoreStories({ projects, headline="Projects" }) {
   return (
     <section>
       <h2 className="mb-8 text-6xl md:text-4xl font-bold tracking-tighter leading-tight">
-        Recently added projects
+        { headline }
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-1 md:col-gap-16 lg:col-gap-32 row-gap-20 md:row-gap-32 mb-32">
         {projects.map(project => (
