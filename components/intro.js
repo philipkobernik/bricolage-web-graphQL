1cc8f7c176c5e832d9f7d6645a27634b0d4e06b8
sam bourgault <samuelle.bourgault@gmail.com>
2020-06-19 12:15:42 -0700

    reposition node navi

--- components/intro.js
+++ components/intro.js
@@ -6,12 +6,12 @@ import BricolageNodeNavigator from '../components/bricolage-nodeNavigator'
 export default function Intro({projects, showNodeNavi=false}) {
   //console.log("intro", projects);
   return (
+    <>
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
 
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         <div className="flex">
           <BricolageLogo />
-          { showNodeNavi && <BricolageNodeNavigator p = {projects} /> }
         </div>
       </h1>
 
@@ -21,6 +21,13 @@ export default function Intro({projects, showNodeNavi=false}) {
         </h4>
       )}
     </section>
+
+    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+    <div className= "mb-8 md:mb-16 -mx-5 sm:mx-0 shadow-small hover:shadow-medium transition-shadow duration-200">
+    { showNodeNavi && <BricolageNodeNavigator p = {projects} /> }
+    </div>
+    </section>
+    </>
 
   )
 }
---
+++ components/lab.js
@@ -0,0 +1,12 @@
+
+export default function Lab({ labs }) {
+
+
+  return (
+      <div className="text-lg my-6">
+        <span className="font-mono font-bold text-yellow">lab.s</span>
+        <br/>
+        <div>{labs}</div>
+      </div>
+  )
+}
