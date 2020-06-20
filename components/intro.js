21fc856f0c4555f5ae01b5f92adc8a6eaa1d4f21
Ari Gilmore <ariellalgilmore@gmail.com>
2020-06-20 12:16:22 -0700

    fix positioning

--- components/intro.js
+++ components/intro.js
@@ -6,18 +6,17 @@ import BricolageNodeNavigator from '../components/bricolage-nodeNavigator'
 export default function Intro({projects=[], showNodeNavi=false}) {
   return (
     <>
-      <section className="flex-col md:flex-row flex items-end  mt-16 mb-16 md:mb-12">
+      <section className="flex-col md:flex-row flex items-end  mt-16 ">
 
         <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
           <div className="flex">
             <BricolageLogo />
           </div>
         </h1>
-
-        <h4 className=" mx-left text-xl">
-          search | disrupt | play | construct
-        </h4>
       </section>
+      <h4 className=" mx-left text-xl mb-16">
+          search | disrupt | play | construct
+      </h4>
 
       <div className= "mb-8 md:mb-16 -mx-5 sm:mx-0 shadow-small hover:shadow-medium transition-shadow duration-200">
         { showNodeNavi && <BricolageNodeNavigator p = {projects} /> }
