a254bfe53d5fa287f862cc1b10519890298755c5
Philip Kobernik <ptk921@gmail.com>
2020-06-20 20:10:18 -0700

    remove subheader for now -- need to figure out how to make sure...
    it doesn't look like navigation UI

--- components/intro.js
+++ components/intro.js
@@ -6,7 +6,7 @@ import BricolageNodeNavigator from '../components/bricolage-nodeNavigator'
 export default function Intro({projects=[], showNodeNavi=false}) {
   return (
     <>
-      <section className="flex-col md:flex-row flex items-end  mt-16 ">
+      <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
 
         <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
           <div className="flex">
@@ -14,9 +14,6 @@ export default function Intro({projects=[], showNodeNavi=false}) {
           </div>
         </h1>
       </section>
-      <h4 className=" mx-left text-xl mt-6 mb-16">
-          search | disrupt | play | construct
-      </h4>
 
       <div className= "mb-8 md:mb-16 -mx-5 sm:mx-0 shadow-small hover:shadow-medium transition-shadow duration-200">
         { showNodeNavi && <BricolageNodeNavigator p = {projects} /> }
