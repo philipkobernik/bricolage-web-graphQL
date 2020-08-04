25deb6d01ad4d6c66881b765f51684465dcf45dd
Philip Kobernik <ptk921@gmail.com>
2020-08-04 10:48:16 -0700

    all video players should have their native controls visible

--- components/video-player.js
+++ components/video-player.js
@@ -4,7 +4,11 @@ export default function VideoPlayer({ videoLink }) {
 
   return (
     <div className="videodiv bg-black grid grid-cols-1 md:grid-cols-4 row-gap-20 md:row-gap-32 mb-32 shadow-small hover:shadow-medium transition-shadow duration-200">
-      <ReactPlayer className= 'react-player md:col-start-1 md:col-end-5' width="100%" height="100%" url={videoLink} />
+      <ReactPlayer
+        className='react-player md:col-start-1 md:col-end-5' width="100%" height="100%"
+        url={videoLink}
+        controls={true}
+      />
     </div>
   )
 
